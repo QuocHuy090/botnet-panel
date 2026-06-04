bb17c24dd681159ec468ce8950973739932f76c9
QuocHuy090 <118003147+QuocHuy090@users.noreply.github.com>
2026-06-05 01:25:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const server = http.createServer(app);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static('public'));
 app.use('/methods', express.static('methods'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') return res.sendStatus(200);
+    next();
+});
 
 let bots = {};
 let pendingCommands = {};
